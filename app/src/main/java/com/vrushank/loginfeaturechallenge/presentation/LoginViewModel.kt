4859164453f2f612d63609130752b1e15fd5ca65
vrushankpatel <patelvd99@gmail.com>
2025-10-07 23:47:42 -0400

    Toast msg fixed

--- app/src/main/java/com/vrushank/loginfeaturechallenge/presentation/LoginViewModel.kt
+++ app/src/main/java/com/vrushank/loginfeaturechallenge/presentation/LoginViewModel.kt
@@ -12,69 +12,75 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
     private val loginUseCase: LoginUseCase,
-    private val logout: Logout,
+private val logout: Logout,
     private val forgotPassword: ForgotPassword
 ) : ViewModel() {
 
 
     val _uiState = MutableStateFlow(LoginDetail())
     val uiState: StateFlow<LoginDetail> = _uiState
-
     val _uiEvent = Channel<UiEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
-
     fun updateEmail(email: String) {
         _uiState.update {
             it.copy(email = email)
         }
     }
-
     fun updatePassword(password: String) {
         _uiState.update {
             it.copy(password = password)
         }
     }
 
-    fun onLogin(email:String, password: String){
+    fun onLogin(email: String, password: String) {
         _uiState.update { it.copy(isLoading = true) }
         viewModelScope.launch {
-            val result = loginUseCase.invoke(email,password)
+            val result = loginUseCase.invoke(email, password)
             _uiState.update {
                 delay(5000L)
                 it.copy(isLoading = false)
             }
-            when(result){
-                is Resources.Error<*> -> {_uiState.update { it.copy(errorMsg = result.error) }}
+            when (result) {
+                is Resources.Error -> {
+//                    _uiState.update { it.copy(errorMsg = result.error) }
+                    _uiEvent.send(UiEvent.ShowToast("Wrong credentials"))
+                }
+
                 is Resources.Loading<*> -> {}
-                is Resources.Success<User> ->  {_uiEvent.send(UiEvent.Success)
-                result.data?.copy()}
+                is Resources.Success<User> -> {
+                    _uiEvent.send(UiEvent.Success)
+                    result.data?.copy()
+                }
 
             }
 
         }
     }
-    fun forgotPasswordClick(){
-        viewModelScope.launch {  _uiEvent.send(UiEvent.ForrgotPassword) }
+
+    fun forgotPasswordClick() {
+        viewModelScope.launch { _uiEvent.send(UiEvent.ForrgotPassword) }
 
     }
 
 
     companion object {
-        fun factory(loginUseCase: LoginUseCase,logout: Logout,forgotPassword: ForgotPassword) = viewModelFactory {
-            addInitializer(LoginViewModel::class) {
-                LoginViewModel(
-                    loginUseCase = loginUseCase,
-                    logout = logout,
-                    forgotPassword = forgotPassword
-                )
+        fun factory(loginUseCase: LoginUseCase, logout: Logout, forgotPassword: ForgotPassword) =
+            viewModelFactory {
+                addInitializer(LoginViewModel::class) {
+                    LoginViewModel(
+                        loginUseCase = loginUseCase,
+                        logout = logout,
+                        forgotPassword = forgotPassword
+                    )
+                }
             }
-        }
     }
 
 }
@@ -83,12 +89,13 @@ data class LoginDetail(
     val email: String = "",
     val password: String = "",
     val isEnabled: Boolean = false,
-    val isLoading: Boolean = false,
-    val errorMsg: String? = ""
+    val isLoading: Boolean = false
 )
 
-sealed class UiEvent(){
-    object Success: UiEvent()
-    object ForrgotPassword: UiEvent()
+sealed class UiEvent() {
+    object Success : UiEvent()
+    class ShowToast(val msg:String): UiEvent()
+
+    object ForrgotPassword : UiEvent()
 
 }
